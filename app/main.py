e827ec67da4436115072ffe20ad01573dd76b18e
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-05-01 15:27:00 +0100

    feat: skeleton out desired endpoints and functionality

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
+import random
+from enum import Enum
 from typing import Tuple
 
 from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import random
-from enum import Enum
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173", # frontend dev
-        "http://localhost:8080", # frontend Docker
+        "http://localhost:5173",  # frontend dev
+        "http://localhost:8080",  # frontend Docker
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -22,6 +22,9 @@ app.add_middleware(
 DRONE_QUANTITY = 5
 DRONE_CENTER = [54.39, -0.937]
 DRONE_STATUSES = ["idle", "flying", "unknown"]
+
+LatLon = Tuple[float, float]
+
 
 @app.get("/")
 async def hello_world():
@@ -36,18 +39,39 @@ class DroneData(BaseModel):
     lastSeen: Tuple[float, float]
 
 
-
-@app.get("/get_drone_status")
+# get the status (position) of all the drones
+# called by the frontend
+@app.get("/drone_status")
 async def get_drone_status():
+    # TODO: send from internal storage
     def create_random_drone(id: str):
         data = DroneData(
             id=id,
             status=str(random.choice(DRONE_STATUSES)),
             battery=random.randint(0, 100),
             lastUpdate="",
-            lastSeen=(DRONE_CENTER[0] + random.random() * 0.05, DRONE_CENTER[1] + random.random() * 0.05)
+            lastSeen=(
+                DRONE_CENTER[0] + random.random() * 0.05,
+                DRONE_CENTER[1] + random.random() * 0.05,
+            ),
         )
         return data
-    drones = [create_random_drone(str(i+1)) for i in range(DRONE_QUANTITY)]
+
+    drones = [create_random_drone(str(i + 1)) for i in range(DRONE_QUANTITY)]
 
     return drones
+
+
+# send the drone fleet to search a particular area
+# currently only supports a circle
+# called by the frontend
+@app.post("/drone_dispatch")
+async def drone_dispatch(target: LatLon, radius: float):
+    pass
+
+
+# notify that a drone has moved
+# called by the drone simulation
+@app.post("/drone_status")
+async def update_drone_status(position: LatLon):
+    pass
