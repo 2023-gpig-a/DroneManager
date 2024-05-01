1230b77a3ab44e5f7e8ce07f6d35441d413f255a
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-05-02 00:01:11 +0100

    feat: implement pathing method 2

--- app/area_resolution.py
+++ app/area_resolution.py
@@ -19,18 +19,36 @@ class TargetCircle(BaseModel):
         num_slices = int(math.ceil(2 * math.pi / slice_angle))
         return [slice_angle * i for i in range(num_slices)]
 
+    def displace_from_centre(self, theta: float, dist: float) -> LatLon:
+        newlat: float = (math.sin(theta) * dist) + self.lat
+        newlon: float = (math.cos(theta) * dist) + self.lon
+        return (newlat, newlon)
+
     def path_method1(self, vision: float = DRONE_VISION) -> list[LatLon]:
         centre: LatLon = (self.lat, self.lon)
-        coords: list[LatLon] = []
+        coords: list[LatLon] = [centre]
+
         for theta in self.slices(vision):
-            newlat: float = (math.sin(theta) * self.radius) + self.lat
-            newlon: float = (math.cos(theta) * self.radius) + self.lon
-            coords.push((newlat, newlon))
-            coords.push(centre)
+            coords.append(self.displace_from_centre(theta, self.radius))
+            coords.append(centre)
+
         return coords
 
     def path_method2(self, vision: float = DRONE_VISION) -> list[LatLon]:
-        pass
+        centre: LatLon = (self.lat, self.lon)
+        coords: list[LatLon] = [centre]
+
+        slices = self.slices(vision)
+
+        for i in range(0, len(slices), 2):
+            theta1 = slices[i]
+            coords.append(self.displace_from_centre(theta1, self.radius))
+            if i + 1 < len(slices):
+                theta2 = slices[i + 1]
+                coords.append(self.displace_from_centre(theta2, self.radius))
+            coords.append(centre)
+
+        return coords
 
     def path_method3(self, vision: float = DRONE_VISION) -> list[LatLon]:
-        pass
+        return []
