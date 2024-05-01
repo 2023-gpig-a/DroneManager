ef59d1f152a12a8c96ff4655a5f3c4df3baf3b6f
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-05-01 16:27:01 +0100

    refactor: constrain types, make path/query/response parameters more clear, add get_next_drone_area endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import random
 from enum import Enum
-from typing import Tuple, Dict, List
+from typing import Dict, List, Tuple
 
 from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
@@ -24,7 +24,6 @@ LatLon = Tuple[float, float]
 
 
 class DroneData(BaseModel):
-    id: DroneId
     status: str
     battery: int
     lastUpdate: str
@@ -43,39 +42,51 @@ async def hello_world():
 # get the status (position) of all the drones
 # called by the frontend
 @app.get("/drone_status")
-async def get_drone_status() -> List[DroneData]:
+async def get_drone_status() -> List[Tuple[DroneId, DroneData]]:
     DRONE_QUANTITY = 5
     DRONE_CENTER = [54.39, -0.937]
 
     # TODO: send from internal storage
-    def create_random_drone(id: DroneId):
-        data = DroneData(
-            id=id,
-            status=str(random.choice(DRONE_STATUSES)),
-            battery=random.randint(0, 100),
-            lastUpdate="",
-            lastSeen=(
-                DRONE_CENTER[0] + random.random() * 0.05,
-                DRONE_CENTER[1] + random.random() * 0.05,
+    def create_random_drone(id: DroneId) -> Tuple[DroneId, DroneData]:
+        return (
+            id,
+            DroneData(
+                status=str(random.choice(DRONE_STATUSES)),
+                battery=random.randint(0, 100),
+                lastUpdate="",
+                lastSeen=(
+                    DRONE_CENTER[0] + random.random() * 0.05,
+                    DRONE_CENTER[1] + random.random() * 0.05,
+                ),
             ),
         )
-        return data
 
-    drones = [create_random_drone(str(i + 1)) for i in range(DRONE_QUANTITY)]
+    return [create_random_drone(str(i + 1)) for i in range(DRONE_QUANTITY)]
 
-    return drones
+
+class TargetCircle(BaseModel):
+    lat: float
+    lon: float
+    radius: float
 
 
 # send the drone fleet to search a particular area
 # currently only supports a circle
 # called by the frontend
 @app.post("/drone_dispatch/circle")
-async def drone_dispatch_circle(target: LatLon, radius: float) -> None:
+async def drone_dispatch_circle(target: TargetCircle) -> None:
     pass
 
 
 # update the stored status of a drone
 # called by the drone simulation
-@app.post("/drone_status")
-async def update_drone_status(drone: DroneData) -> None:
+@app.post("/drone_status/{id}")
+async def update_drone_status(id: DroneId, drone: DroneData) -> None:
+    pass
+
+
+# retrieve the next sequence of points that a drone needs to go and "scan"
+# called by the drone simulation
+@app.get("/next_area")
+async def get_next_drone_area() -> List[LatLon]:
     pass
