3673fcc4ff9fa9b316de3d84118607dc5e11c5df
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-05-02 14:19:59 +0000

    Skeleton the desired endpoints (#8)
    * chore: add a dockerignore and a gitignore file
    
    * feat: skeleton out desired endpoints and functionality
    
    * refactor: reorganise and tighten spec for endpoints
    
    * refactor: constrain types, make path/query/response parameters more clear, add get_next_drone_area endpoint
    
    * fix: remove unused imports

--- app/main.py
+++ app/main.py
@@ -1,53 +1,91 @@
-from typing import Tuple
+import random
+from typing import Dict, List, Tuple
 
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import random
-from enum import Enum
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173", # frontend dev
-        "http://localhost:8080", # frontend Docker
+        "http://localhost:5173",  # frontend dev
+        "http://localhost:8080",  # frontend Docker
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-DRONE_QUANTITY = 5
-DRONE_CENTER = [54.39, -0.937]
+DroneId = str
+LatLon = Tuple[float, float]
+
+
+class DroneData(BaseModel):
+    status: str
+    battery: int
+    lastUpdate: str
+    lastSeen: LatLon
+
+
 DRONE_STATUSES = ["idle", "flying", "unknown"]
+DRONES: Dict[DroneId, DroneData] = dict()
+
 
 @app.get("/")
 async def hello_world():
     return {"response": "Hello World"}
 
 
-class DroneData(BaseModel):
-    id: str
-    status: str
-    battery: int
-    lastUpdate: str
-    lastSeen: Tuple[float, float]
+# get the status (position) of all the drones
+# called by the frontend
+@app.get("/drone_status")
+async def get_drone_status() -> List[Tuple[DroneId, DroneData]]:
+    DRONE_QUANTITY = 5
+    DRONE_CENTER = [54.39, -0.937]
 
-
-
-@app.get("/get_drone_status")
-async def get_drone_status():
-    def create_random_drone(id: str):
-        data = DroneData(
-            id=id,
-            status=str(random.choice(DRONE_STATUSES)),
-            battery=random.randint(0, 100),
-            lastUpdate="",
-            lastSeen=(DRONE_CENTER[0] + random.random() * 0.05, DRONE_CENTER[1] + random.random() * 0.05)
+    # TODO: send from internal storage
+    def create_random_drone(id: DroneId) -> Tuple[DroneId, DroneData]:
+        return (
+            id,
+            DroneData(
+                status=str(random.choice(DRONE_STATUSES)),
+                battery=random.randint(0, 100),
+                lastUpdate="",
+                lastSeen=(
+                    DRONE_CENTER[0] + random.random() * 0.05,
+                    DRONE_CENTER[1] + random.random() * 0.05,
+                ),
+            ),
         )
-        return data
-    drones = [create_random_drone(str(i+1)) for i in range(DRONE_QUANTITY)]
 
-    return drones
+    return [create_random_drone(str(i + 1)) for i in range(DRONE_QUANTITY)]
+
+
+class TargetCircle(BaseModel):
+    lat: float
+    lon: float
+    radius: float
+
+
+# send the drone fleet to search a particular area
+# currently only supports a circle
+# called by the frontend
+@app.post("/drone_dispatch/circle")
+async def drone_dispatch_circle(target: TargetCircle) -> None:
+    pass
+
+
+# update the stored status of a drone
+# called by the drone simulation
+@app.post("/drone_status/{id}")
+async def update_drone_status(id: DroneId, drone: DroneData) -> None:
+    pass
+
+
+# retrieve the next sequence of points that a drone needs to go and "scan"
+# called by the drone simulation
+@app.get("/next_area")
+async def get_next_drone_area() -> List[LatLon]:
+    pass
