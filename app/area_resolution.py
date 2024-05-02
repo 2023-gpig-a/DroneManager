b2a864e02a0e10c4574a9a46018dad4af6d91146
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-05-02 14:50:30 +0100

    doc: endpoint documentation and docstrings

--- app/area_resolution.py
+++ app/area_resolution.py
@@ -9,22 +9,35 @@ DRONE_VISION = 1.0
 
 
 class TargetCircle(BaseModel):
+    """Circle centred around a lat/lon with a radius.
+
+    Has methods for routing drones to cover it, given a particular (circular) vision radius.
+    """
+
     lat: float
     lon: float
     radius: float
 
     def slice_angles(self, vision: float) -> list[float]:
+        """Get the maximum angle to divide the circle into arcs.
+
+        An angle will be given such that `vision` is the length of the curved side of the arc(s).
+        """
         inc_angle = math.atan2(vision * 2, self.radius)
         # TODO: does this want to be floor or ceil?
         num_slices = int(math.ceil(2 * math.pi / inc_angle))
         return [inc_angle * i for i in range(num_slices)]
 
     def displace_from_centre(self, theta: float, dist: float) -> LatLon:
+        """Get a point given a bearing and distance from the centre."""
+
         newlat: float = (math.sin(theta) * dist) + self.lat
         newlon: float = (math.cos(theta) * dist) + self.lon
         return (newlat, newlon)
 
     def path_method1(self, vision: float = DRONE_VISION) -> list[LatLon]:
+        """Generate a radial point sequence."""
+
         centre: LatLon = (self.lat, self.lon)
         coords: list[LatLon] = [centre]
 
@@ -35,6 +48,8 @@ class TargetCircle(BaseModel):
         return coords
 
     def path_method2(self, vision: float = DRONE_VISION) -> list[LatLon]:
+        """Generate a radial point sequence, with lower overall distance."""
+
         centre: LatLon = (self.lat, self.lon)
         coords: list[LatLon] = [centre]
 
@@ -51,4 +66,5 @@ class TargetCircle(BaseModel):
         return coords
 
     def path_method3(self, vision: float = DRONE_VISION) -> list[LatLon]:
+        """Generate a squared zig-zag across the circle."""
         return []
--- app/main.py
+++ app/main.py
@@ -9,7 +9,25 @@ from pydantic import BaseModel
 from .area_resolution import TargetCircle
 from .types import DroneId, LatLon
 
-app = FastAPI()
+app = FastAPI(
+    title="Drone Manager",
+    summary="Middle layer between frontend systems and manufacturer drone interfaces.",
+    description="""Drone Manager is a service which interfaces between abstracted frontend systems which wish to instruct drones to photograph particular areas (and get regular updates), and the systems provided by drone manufacturers to allow remote instruction of drone flight controllers.
+It is intended to provide an abstraction layer over different drone models.
+
+Endpoints are provided for the frontend systems to call to instruct the fleet and get updates on their positions and progress; these are:
+
+- `GET /drone_status*`
+- `POST /drone_dispatch*`
+
+Endpoints for drone controllers are:
+
+- `POST /drone_status*`
+- `GET /next_area`
+
+See respective endpoints for full documentation.
+    """,
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -37,19 +55,25 @@ DRONE_VISION = 1.0
 ROUTES_QUEUE: list[list[LatLon]] = []
 
 
-@app.get("/")
+@app.get("/", summary="Hello world sanity check.")
 async def hello_world():
     return {"response": "Hello World"}
 
 
-# get the status (position) of all the drones
-# called by the frontend
-@app.get("/drone_status")
+@app.get(
+    "/drone_status",
+    summary="Get all known information about all drones in the field.",
+    description="Intended for frontend usage.",
+)
 async def get_drone_status() -> dict[DroneId, DroneData]:
     return DRONES
 
 
-@app.get("/drone_status/{id}")
+@app.get(
+    "/drone_status/{id}",
+    summary="Get all known information about a particular drone, given it's id.",
+    description="Intended for frontend usage.",
+)
 async def get_individual_drone(id: DroneId) -> DroneData:
     # TODO: handle KeyErrors better
     return DRONES[id]
@@ -58,18 +82,22 @@ async def get_individual_drone(id: DroneId) -> DroneData:
 ROUTING_METHOD = TargetCircle.path_method1
 
 
-# send the drone fleet to search a particular area
-# currently only supports a circle
-# called by the frontend
-@app.post("/drone_dispatch/circle")
+@app.post(
+    "/drone_dispatch/circle",
+    summary="Add a circular area to the queue for drones to search.",
+    description="Intended for frontend usage.",
+)
 async def drone_dispatch_circle(target: TargetCircle) -> None:
     ROUTES_QUEUE.append(ROUTING_METHOD(target, DRONE_VISION))
     return
 
 
-# update the stored status of a drone
-# called by the drone simulation
-@app.post("/drone_status/{id}")
+@app.post(
+    "/drone_status/{id}",
+    summary="Update the known status of a drone.",
+    description="""This updates the internal cache of this service, and will be served with the `GET /drone_status` endpoints.
+Intended for backend usage.""",
+)
 async def update_drone_status(id: DroneId, drone: DroneData) -> None:
     DRONES[id] = drone
     return
@@ -77,6 +105,11 @@ async def update_drone_status(id: DroneId, drone: DroneData) -> None:
 
 # retrieve the next sequence of points that a drone needs to go and "scan"
 # called by the drone simulation
-@app.get("/next_area")
+@app.get(
+    "/next_area",
+    summary="Retrieve the next sequence of points in the queue for a drone to photograph.",
+    description="""**NOTE**: this sequence of points is the *minimal* routing path.
+The drone **must** self-instruct on when photographs are taken, eg. whenever it's out of range of the last photo taken, instead of just taking one at each node.""",
+)
 async def get_next_drone_area() -> list[LatLon]:
     return ROUTES_QUEUE.pop()
