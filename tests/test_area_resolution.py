89507178eca9e6ca39b31ca31d4cbf2a0139eedc
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-05-02 16:46:46 +0100

    test: first unit test :D

--- tests/test_area_resolution.py
+++ tests/test_area_resolution.py
@@ -1,7 +1,73 @@
 import unittest
-from app import area_resolution
+import math
+from app.area_resolution import TargetCircle
+from app.types import LatLon
 
 
-class TestCircleRoutingMethod1(unittest.TestCase):
-    def test_sanity(self):
+class TestCircleRouting(unittest.TestCase):
+    def assert_latlon_lists_equal(
+        self,
+        left: list[LatLon],
+        right: list[LatLon],
+        rel_tol=0.001,
+    ):
+        self.assertEqual(len(left), len(right))
+        for pair in zip(left, right):
+            l, r = pair
+            self.assertTrue(math.isclose(l[0], r[0], rel_tol=rel_tol))
+            self.assertTrue(math.isclose(l[1], r[1], rel_tol=rel_tol))
+
+    def assert_path_expected(
+        self,
+        circle: TargetCircle,
+        vision: float,
+        # TODO: what do function types look like in python??
+        method,
+        expected: list[LatLon],
+    ):
+        path = method(circle, vision)
+        self.assert_latlon_lists_equal(path, expected)
+
+
+class TestCircleRoutingMethod1(TestCircleRouting):
+    def test1(self):
+        centre = (10.0, 10.0)
+        self.assert_path_expected(
+            circle=TargetCircle(lat=centre[0], lon=centre[1], radius=20.0),
+            vision=5.0,
+            method=TargetCircle.path_method1,
+            expected=[
+                centre,
+                (10.0, 30.0),
+                centre,
+                (18.94427190999916, 27.88854381999832),
+                centre,
+                (26.0, 22.0),
+                centre,
+                (29.677398201998148, 13.577708763999667),
+                centre,
+                (29.200000000000003, 4.400000000000002),
+                centre,
+                (24.668605932398624, -3.5952933031987158),
+                centre,
+                (17.040000000000006, -8.719999999999999),
+                centre,
+                (7.9249289168802, -9.892060727838128),
+                centre,
+                (-0.7519999999999971, -6.864000000000001),
+                centre,
+                (-7.158691232142381, -0.27517957020704387),
+                centre,
+                (-9.9424, 8.48319999999999),
+                centre,
+                (-8.515358395451063, 17.56184524358968),
+                centre,
+                (-3.1788800000000084, 25.043839999999992),
+                centre,
+                (4.940261157601107, 29.34939386251466),
+                centre,
+            ],
+        )
+
+    def test2(self):
         pass
